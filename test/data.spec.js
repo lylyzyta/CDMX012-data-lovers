fa5f72384bf9e2fd1a2f5ac61106fa4f2dd7864d
lylyzyta <lylyzyta@gmail.com>
2022-02-24 21:15:24 -0600

    Agregué la plantilla para los test de las funciones

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,69 @@
-import { example, anotherExample } from '../src/data.js';
+import {topCinco, imprimePersonajes, personajesfilter, caracteristicafilter, specieFilter, edadFilter} from '../src/data.js';
 
-
-describe('example', () => {
+//Test Función topCinco
+describe('topCinco', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof topCinco).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
+  it('returns `topCinco`', () => {
     expect(anotherExample()).toBe('OMG');
   });
 });
+
+//Test Función imprimePersonajes
+describe('imprimePersonajes', () => {
+  it('is a function', () => {
+    expect(typeof imprimePersonajes).toBe('function');
+  });
+
+  it('returns `imprime todos los personajes`', () => {
+    expect(anotherExample()).toBe('OMG');
+  });
+});
+
+//Test Función personajesfilter
+describe('personajesfilter', () => {
+  it('is a function', () => {
+    expect(typeof personajesfilter).toBe('function');
+  });
+
+  it('returns `personajes por película`', () => {
+    expect(anotherExample()).toBe('OMG');
+  });
+});
+
+//Test Función caracteristicafilter
+describe('caracteristicafilter', () => {
+  it('is a function', () => {
+    expect(typeof caracteristicafilter).toBe('function');
+  });
+
+  it('returns `género de personaje`', () => {
+    expect(anotherExample()).toBe('OMG');
+  });
+});
+
+//Test Función specieFilter
+describe('specieFilter', () => {
+  it('is a function', () => {
+    expect(typeof specieFilter).toBe('function');
+  });
+
+  it('returns `Clasifica Especie`', () => {
+    expect(anotherExample()).toBe('OMG');
+  });
+});
+
+
+//Test Función edadFilter
+describe('edadFilter', () => {
+  it('is a function', () => {
+    expect(typeof edadFilter).toBe('function');
+  });
+
+  it('returns `Clasifica Edad`', () => {
+    expect(anotherExample()).toBe('OMG');
+  });
+});
+
